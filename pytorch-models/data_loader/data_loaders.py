046bdb74c72697fc7c4f30612c055324a86330cd
hanlin <hanlaung@stanford.edu>
2019-10-20 00:23:53 -0700

    Added data loader for bio relations

--- pytorch-models/data_loader/data_loaders.py
+++ pytorch-models/data_loader/data_loaders.py
@@ -15,4 +15,14 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
-        
+
+
+class BioDataLoader(BaseDataLoader):
+    """
+    Data loader for biology relations
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        self.data_dir = data_dir
+        self.dataset = RelationDataSet(self.data_dir, train=training)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
