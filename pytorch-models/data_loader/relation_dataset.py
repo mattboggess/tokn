4c0ed661c4ad0105b893bc1ec91e7a30f5bb44dd
hanlin <hanlaung@stanford.edu>
2019-10-21 11:24:11 -0700

    Added skeleton of relation datasets

--- pytorch-models/data_loader/relation_dataset.py
+++ pytorch-models/data_loader/relation_dataset.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 from relation_constants import ALL_RELATIONS
 
 class RelationDataset(Dataset):
-    """Face Landmarks dataset."""
+    """Relations dataset."""
 
     def __init__(self, json_file, relations=ALL_RELATIONS, transform=None):
         """
@@ -18,11 +18,57 @@ class RelationDataset(Dataset):
         unused_relations = [relation for relation in relations_dict.keys() if relation not in relations]
         for unused_relation in unused_relations:
             all_relations.pop(unused_relation)
-        self.relations = all_relations.values()
+        self.relations = list(all_relations.values())
         self.transform = transform
 
     def __len__(self):
         return sum([len(self.relations) for relation in self.relations])
 
     def __getitem__(self, idx):
-        pass 
+        pass
+
+
+
+class TaxonomyRelationDataset(Dataset):
+    """Taxonomy relations dataset."""
+
+    def __init__(self, json_file, relations=ALL_RELATIONS, transform=None):
+        """
+        Args:
+            json_file (string): Path to the json file with relations.
+        """
+        all_relations = json.load(json_file)
+        unused_relations = [relation for relation in relations_dict.keys() if relation not in TAXONOMY RELATIONS]
+        for unused_relation in unused_relations:
+            all_relations.pop(unused_relation)
+        self.relations = list(all_relations.values())
+        self.transform = transform
+
+    def __len__(self):
+        return sum([len(self.relations) for relation in self.relations])
+
+    def __getitem__(self, idx):
+        pass
+
+
+
+class MeronymRelationDataset(Dataset):
+    """Meronymic relations dataset."""
+
+    def __init__(self, json_file, relations=ALL_RELATIONS, transform=None):
+        """
+        Args:
+            json_file (string): Path to the json file with relations.
+        """
+        all_relations = json.load(json_file)
+        unused_relations = [relation for relation in relations_dict.keys() if relation not in STRUCTURE_RELATIONS]
+        for unused_relation in unused_relations:
+            all_relations.pop(unused_relation)
+        self.relations = list(all_relations.values())
+        self.transform = transform
+
+    def __len__(self):
+        return sum([len(self.relations) for relation in self.relations])
+
+    def __getitem__(self, idx):
+        pass
