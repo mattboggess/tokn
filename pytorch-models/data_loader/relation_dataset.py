5d7c933455d7daa8c501748fb293de7757abfc56
hanlin <hanlaung@stanford.edu>
2019-10-22 21:27:14 -0700

    Fixed errors to retrieve relation based on vocab file

--- pytorch-models/data_loader/relation_dataset.py
+++ pytorch-models/data_loader/relation_dataset.py
@@ -8,15 +8,16 @@ from torch.utils.data import Dataset, DataLoader
 from relation_constants import ALL_RELATIONS
 
 class RelationDataset(Dataset):
-    """Face Landmarks dataset."""
+    """Relation dataset."""
 
     def __init__(self, data_dir, relations=ALL_RELATIONS):
         """
         Args:
-            json_file (string): Path to the json file with relations.
+            data_dir (string): Path to the data directory with relations_db json file and words.txt file for vocab2id generation
+            relations (string): list of relations 
         """
-        all_relations = json.load(os.path.join(data_dir, "relations_db.json"))
-        unused_relations = [relation for relation in relations_dict.keys() if relation not in relations]
+        all_relations = json.load(open(os.path.join(data_dir, "relations_db.json")))
+        unused_relations = [relation for relation in all_relations.keys() if relation not in relations]
         for unused_relation in unused_relations:
             all_relations.pop(unused_relation)
         self.relation_df = None 
@@ -25,12 +26,9 @@ class RelationDataset(Dataset):
             if self.relation_df is None:
                 self.relation_df = df
             else:
-                self.relation_df = pd.concat([self.relation_df, df])
+                self.relation_df = pd.concat([self.relation_df, df], sort=False)
         self.data_dir = data_dir
-
-        vocab_path = os.path.join(data_dir, 'words.txt')
-        self.vocab2id = {v: id for v, id in self.vocab2id.items()}
-
+        self.vocab2id = json.load(open(os.path.join(data_dir, 'word2id.json')))
 
     def __len__(self):
         return self.relation_df.size 
@@ -38,11 +36,11 @@ class RelationDataset(Dataset):
     def __getitem__(self, idx):
         sample = self.relation_df.iloc[idx]
         y_label = sample['relation']
-        bag_of_words = [preprocess(sentence) for sentence in sample['sentences']]
+        bag_of_words = [self.preprocess(sentence) for sentence in sample['sentences']]
         return bag_of_words, y_label 
 
 
-    def preprocess(sentence_tokenized):
-        sequence = [self.vocab2id(token) for token in sentence_tokenized]
+    def preprocess(self, sentence_tokenized):
+        sequence = [self.vocab2id[token.lower()] if token in self.vocab2id else self.vocab2id['<unk>'] for token in sentence_tokenized]
         sequence = torch.Tensor(sequence)
         return sequence
