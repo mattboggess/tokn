7bdb16c724dd8c1cb9c20dd6c807722038ed87d8
hanlin <hanlaung@stanford.edu>
2019-11-03 20:03:23 -0800

    Added bag of words featurizer

--- utils/feature_utils.py
+++ utils/feature_utils.py
@@ -1,6 +1,16 @@
-def bag_of_words_featurizer(term_pair, feature_counter):
-    sentences = term_pair['sentences']
-    #sentences.find("<e1>") and indices to get middle
-    for word in ex.middle.split(' '):
-        feature_counter[word] += 1
-    return feature_counter
+import json 
+import numpy as np
+
+ID2WORD = json.load(open(os.path.join("data/", 'id2word.json')))
+
+def bag_of_words_featurizer(bag_of_words):
+    """
+    Featurizes bag of words into a vector based on word count
+    bag_of_words is a list of lists of word ids
+    """
+    feat_len = len(ID2WORD)
+    feat_vec = np.zeros(feat_len)
+    for sentence in bag_of_words:
+        for word_id in sentence:
+            feat_vec[word_id] += 1
+    return feat_vec
