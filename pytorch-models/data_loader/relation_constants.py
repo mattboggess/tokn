3f3fd0721f4c818149cb39f0e24b96c50802fd16
hanlin <hanlaung@stanford.edu>
2019-12-13 01:03:13 -0800

    Updated utils and delted outer pytorch-models

--- pytorch-models/data_loader/relation_constants.py
+++
@@ -1,3 +0,0 @@
-ALL_RELATIONS = ['no-relation', 'subclass-of', 'has-part', 'possesses', 'has-region', 'is-inside', 'is-at', 'element', 'abuts', 'is-outside']
-TAXONOMY_RELATIONS = ['subclass-of']
-STRUCTURE_RELATIONS = ['has-part', 'possesses', 'has-region', 'is-inside', 'is-at', 'element', 'abuts', 'is-outside']
