6801e19575d5838f38467b201007712dbe0d0c30
mattboggess <mattboggess@mailfence.com>
2020-04-11 19:16:50 -0700

    make term source selection more customizable

--- data_processing/tag_sentences.py
+++ data_processing/tag_sentences.py
@@ -1,13 +1,13 @@
 # Tags sentences with key terms from textbooks in preparation for term extraction modeling. 
 
 # Author: Matthew Boggess
-# Version: 4/3/20
+# Version: 4/11/20
 
-# Data Source: Output of preprocess_openstax_textbooks.py and preprocess_life_biology.py
-# scripts.
+# Data Source: Output of preprocess_openstax_textbooks.py, preprocess_life_biology.py, and
+# preprocess_hand-labeled_sections.py scripts.
 
 # Description: 
-#   For each provided textbook: 
+#   For each provided textbook/textbook section: 
 #     - Reads in spacy preprocessed terms from all specified term sources and aggregates them
 #       all into a single list of terms to tag the textbooks with (special exception is that
 #       the life biology kb is tagged on its own)
@@ -27,8 +27,7 @@
 
 # Libraries 
 
-import stanfordnlp
-from spacy_stanfordnlp import StanfordNLPLanguage
+import spacy
 from data_processing_utils import tag_terms, read_spacy_docs
 from collections import Counter
 import warnings
@@ -49,62 +48,54 @@ if not os.path.exists(output_data_dir):
 
 ## Other parameters 
 
-# textbooks whose sentences we would like to tag
-textbooks = [
+# list of all term sources we want to combine to create a large list for tagging
+# we exclude the Life Biology KB terms from this aggregate since the nature of the
+# kb leads to many generic descriptions of terms that we don't want to generally tag (i.e. group)
+pooled_terms = [
     'Anatomy_and_Physiology', 
     'Astronomy', 
     'Biology_2e',
     'Chemistry_2e',
-    'Life_Biology_kb',
     'Microbiology',
     'Psychology',
     'University_Physics_Volume_1',
     'University_Physics_Volume_2',
-    'University_Physics_Volume_3'
+    'University_Physics_Volume_3',
+    'openstax_bio2e_section10-2'
 ]
 
-# textbooks whose key terms should be aggregated together to create a full term list for tagging 
-term_sources = [
-    'Anatomy_and_Physiology', 
-    'Astronomy', 
-    'Biology_2e',
-    'Chemistry_2e',
-    'Microbiology',
-    'Psychology',
-    'University_Physics_Volume_1',
-    'University_Physics_Volume_2',
-    'University_Physics_Volume_3'
-]
+# mapping from textbooks/textbook sections whose sentences we would like to tag to the
+# sources of terms we should use for tagging them
+textbooks = {
+    'openstax_bio2e_section10-2': ['openstax_bio2e_section10-2']
+    'Anatomy_and_Physiology': pooled_terms, 
+    'Astronomy': pooled_terms, 
+    'Biology_2e': pooled_terms,
+    'Chemistry_2e': pooled_terms,
+    'Life_Biology_kb': ['Life_Biology_kb'],
+    'Microbiology': pooled_terms,
+    'Psychology': pooled_terms,
+    'University_Physics_Volume_1': pooled_terms,
+    'University_Physics_Volume_2': pooled_terms,
+    'University_Physics_Volume_3': pooled_terms
+}
 
 #===================================================================================
 
 if __name__ == '__main__':
     
-    warnings.filterwarnings('ignore')
-    snlp = stanfordnlp.Pipeline(lang="en")
-    nlp = StanfordNLPLanguage(snlp)
+    #warnings.filterwarnings('ignore')
+    nlp = spacy.load("en_core_web_sm")
     
-    # aggregate key terms into single list
-    agg_terms = []
-    for term_source in term_sources:
-        agg_terms += read_spacy_docs(
-            f"{input_data_dir}/{term_source}_key_terms_spacy",
-            f"{input_data_dir}/{term_source}_key_terms_spacy_vocab"
-        )
-    
-    for i, textbook in enumerate(textbooks):
-        print(f"Tagging {textbook} textbook sentences: Textbook {i + 1}/{len(textbooks)}")
+    for i, (textbook, term_sources) in enumerate(textbooks.items()):
+        print(f"Tagging {textbook} sentences: Input {i + 1}/{len(textbooks)}")
         
-        # special case handling of the life bio knowledge base, we use it specifically for
-        # evaluation, but don't include its terms generally since there are many too general
-        # word representations for bio concepts in the knowledge base
-        if textbook == 'Life_Biology_kb':
-            terms = read_spacy_docs(
-                f"{input_data_dir}/Life_Biology_kb_key_terms_spacy",
-                f"{input_data_dir}/Life_Biology_kb_key_terms_spacy_vocab"
+        terms = []
+        for term_source in term_sources:
+            terms += read_spacy_docs(
+                f"{input_data_dir}/{term_source}_key_terms_spacy",
+                f"{input_data_dir}/{term_source}_key_terms_spacy_vocab"
             )
-        else:
-            terms = agg_terms
     
         tagged_sentences = []
         spacy_sentences = read_spacy_docs(
